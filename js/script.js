f879966dc2a4b4a37b40a0ca54485367288152c0
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-02-08 13:23:36 +0100

    done

--- js/script.js
+++ js/script.js
@@ -4,6 +4,18 @@ stampale in una lista */
 var app = new Vue({
     el: '#app',
     data: {
+        emails: []
+    },
+    mounted() {
+        //Ciclo per richiamare 10 volte
+        for (let i = 0; i < 10; i++){
 
+            //Chiamata API e-mail random e push in array
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then( result => {
+                    this.emails.push(result.data.response)
+                });
+        }
     }
 });
